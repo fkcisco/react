15396c2acbb13e79bebce0b4d803539fc413f9be
kcisco <kciscok@gmail.com>
2022-05-31 19:01:11 -0300

    tercer desafio 1.0

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,15 +1,13 @@
 import {Container } from 'react-bootstrap'
 
 const Contenido= () => {
-    return (
+    return (    
     
-    <header className="App-header">  
         <Container>        
             <p>
             Hola soy una <code>App.js</code> desarrolla en React que de a poco me voy a convertir en un Ecommerce dinámico sin la necesidad de usar wordpress y sus mil millones de peticiones al servidor :D
             </p> 
-        </Container>            
-    </header>
+        </Container>
 
 )
 }
